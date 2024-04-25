78b5d8b2a1f8985e21bdda48d866cff2e1fc5696
vish <sali.vishwaraj@gmail.com>
2024-04-24 23:56:15 -0400

    Base application

---
+++ src/main/java/dev/vishsali/inboxapp/InboxAppApplication.java
@@ -0,0 +1,13 @@
+package dev.vishsali.inboxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InboxAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vishsali/inboxapp/InboxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vishsali.inboxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InboxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
